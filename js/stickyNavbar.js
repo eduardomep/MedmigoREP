c392941ac1af25789588cd0bfe67b5b4e4862fd8
Your Name <you@example.com>
2020-04-20 15:18:44 -0600

    features and success

--- js/stickyNavbar.js
+++ js/stickyNavbar.js
@@ -1,22 +1,22 @@
 'use strict'
 
-// window.addEventListener('scroll',changeNavbar);
+window.addEventListener('scroll',changeNavbar);
 
-// function changeNavbar() {
-//     let scrollPostionY = window.scrollY;
-//     let navbar = document.querySelector('.navbar');
+function changeNavbar() {
+    let scrollPostionY = window.scrollY;
+    let navbar = document.querySelector('.navbar');
     
-//     if(scrollPostionY>50){
-//         navbar.classList.add('bg-white')
-//         navbar.classList.add('navbar-light')
-//         navbar.classList.remove('bg-transparent')
-//         navbar.classList.remove('navbar-dark')
-//     }
-//     else{
-//         navbar.classList.remove('bg-white')
-//         navbar.classList.remove('navbar-light')
-//         navbar.classList.add('bg-transparent')
-//         navbar.classList.add('navbar-dark')
-//     }
-// }
+    if(scrollPostionY>50){
+        navbar.classList.add('bg-white')
+        navbar.classList.add('navbar-light')
+        navbar.classList.remove('bg-transparent')
+        navbar.classList.remove('navbar-dark')
+    }
+    else{
+        navbar.classList.remove('bg-white')
+        navbar.classList.remove('navbar-light')
+        navbar.classList.add('bg-transparent')
+        navbar.classList.add('navbar-dark')
+    }
+}
 
